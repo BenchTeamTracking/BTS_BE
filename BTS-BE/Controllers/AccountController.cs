933f95c75f510e8938b52a76b67c5a7374832d29
smakilan <stephensmakilan@gmail.com>
2024-04-23 08:36:25 +0800

    initial commit

---
+++ BTS-BE/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using BTS_BE.Controllers.Api;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTS_BE.Controllers
+{
+    [Authorize]
+    public class AccountController: BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BTS-BE/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using BTS.Domain.Core.User;
+using BTS.Infrastructure.Repositories;
+using BTS_BE.Controllers.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTS_BE.Controllers
+{
+    public class UserController: BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Get user information
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var result = _userRepository.GetItemByIdAsync(id);
+            return Ok();
+        }
+    }
+}
